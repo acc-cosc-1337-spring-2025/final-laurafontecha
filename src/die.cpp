c8e67902c21dc41d46967dbf98fbe79082f745d9
laurafontecha <laura.fontechaleon@g.austincc.edu>
2025-05-18 00:59:51 +0000

    Question 1

--- src/die.cpp
+++ src/die.cpp
@@ -1,1 +1,15 @@
-//
+#include "die.h"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+int Die::roll()
+{
+    static bool seeded= false;
+    if (!seeded)
+    {
+        std::srand(static_cast<unsigned int> (std::time(nullptr)));
+        seeded = true;
+    }
+    return (std::rand()% sides)+1;
+}
--- test/craps_test.cpp
+++ test/craps_test.cpp
@@ -1,6 +1,16 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "die.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
+}
+
+TEST_CASE("Die roll returns value between 1 and 6 ") {
+	Die die;
+	for(int i=0; i<10; i++){
+		int result=die.roll();
+		REQUIRE(result >= 1);
+		REQUIRE (result <= 6);
+	}
 }
